c38aa58b2efa56b4a2e6b8207c89d32fe276e3f5
jhoncharry <jhon.charryserrato@gmail.com>
2021-08-07 16:45:12 -0500

    update client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
 export default ({ req }) => {
-    if (typeof window === 'undefined') {
-        // we are on the server!
-        // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-        return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-            headers: req.headers
-        });
-    } else {
-        // we must be on the browser!
-        // requests can be made with a base url of ''
-        return axios.create({
-            baseURL: '/'
-        });
-    }
+    // we must be on the browser!
+    // requests can be made with a base url of ''
+    return axios.create({
+        baseURL: '/'
+    });
+
+    /*     if (typeof window === 'undefined') {
+            // we are on the server!
+            // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+            return axios.create({
+                baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+                headers: req.headers
+            });
+        } else {
+            // we must be on the browser!
+            // requests can be made with a base url of ''
+            return axios.create({
+                baseURL: '/'
+            });
+        } */
 }
