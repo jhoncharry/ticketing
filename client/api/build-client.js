2e23bf1d528fd05d06afd9bc2309adde7a283091
jhoncharry <jhon.charryserrato@gmail.com>
2021-08-07 18:09:30 -0500

    client updated

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default ({ req }) => {
+    console.log("xxxxxxxxxxxxxxxxxxxx33333333333333333");
     if (typeof window === 'undefined') {
         // we are on the server!
         // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
@@ -9,6 +10,7 @@ export default ({ req }) => {
             headers: req.headers
         });
     } else {
+        console.log("xxxxxxxxxxxxxxxxxxxx44444444444444444");
         // we must be on the browser!
         // requests can be made with a base url of ''
         return axios.create({
