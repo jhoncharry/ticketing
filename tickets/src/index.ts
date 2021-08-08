9798547afdbce10be35d0bf3314453ad2480e2cc
jhoncharry <jhon.charryserrato@gmail.com>
2021-08-07 22:33:41 -0500

    some commit

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
-    console.log("Starting...");
+    console.log("Starting... *-*_");
     if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
     if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined");
